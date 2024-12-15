24b29da6dbb5f8eb3e24ab51dde55a52a014fa7d
mtencza00 <michael_tencza@drivosity.com>
2024-12-15 17:36:23 -0500

    init trouble

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -85,6 +85,8 @@ require('lazy').setup({
 
   require 'kickstart/plugins/nvim-ts-autotag',
 
+  require 'kickstart/plugins/trouble',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
